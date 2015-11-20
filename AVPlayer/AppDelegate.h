67c27a70d710d7f67f4e6fe53e9bf71ba20eaad6
icharlie2014 <plyoujian@163.com>
2015-11-20 16:27:19 +0800

    player

---
+++ AVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by panglong on 15/11/19.
+//  Copyright © 2015年 庞龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
